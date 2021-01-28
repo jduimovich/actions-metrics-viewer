cae78374d0deb0ab4403f6d771bc5386f987997c
John Duimovich <jduimovich@gmail.com>
2021-01-28 13:02:39 -0500

    fix names

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --rm --name nginx -p 8888:80 -v %CD%:/usr/share/nginx/html:ro -d docker.io/nginx
-start http://localhost:8888
+docker run --rm --name nginx -p 8888:80 -v $(pwd):/usr/share/nginx/html:ro -d docker.io/nginx
+open http://localhost:8888
